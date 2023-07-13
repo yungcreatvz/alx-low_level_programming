42c52802bd04bbc2ad0226594d4ef23315ea30b0
pyfn <pyfn94@gmail.com>
2023-07-13 12:34:02 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
  * main - prints exactly w/ printf or puts
  * text "nd that piece of art is useful`\" - Dora Korpar, 2015-10-19"
  * followed by a new line
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "nd that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "nd that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
