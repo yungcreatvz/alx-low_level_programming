1dde2213800b1f3d9f73019ae4dcd77d3a1faf3e
pyfn <pyfn94@gmail.com>
2023-07-13 10:29:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  * followed by a new line
  * Return: Always 0 (Success)
  */
-int main(void)
+int main(void)	
 {
-  	puts("\"Programming is like building a multilingual puzzle");
-      	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
