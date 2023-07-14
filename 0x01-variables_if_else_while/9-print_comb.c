538084b61389455532ac401e86117b2954dd6e54
pyfn <pyfn94@gmail.com>
2023-07-14 15:20:01 -0700

    100-Print-comb3

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,30 +3,23 @@
 #include <stdio.h>
 
 /**
- * main - Print comb
+ * main - Print Alphabet
  * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int a;
-	int b;
 
-	for (a = 48; a < 57; a++)
+	for (a = 48; a <= 57; a++)
 	{
-		for (b = 48; b <= 57; b++)
+		putchar(a);
+		if (a != 57)
 		{
-			if (a != b)
-			{
-				putchar(a);
-				putchar(b);
-				if (a != 56 || b != 57)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
+	return (0);
 }
