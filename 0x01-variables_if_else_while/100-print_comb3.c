760cbbc1269e8ccdc6b2f94da92423a53c319485
pyfn <pyfn94@gmail.com>
2023-07-14 15:17:01 -0700

    9-Print-comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print Alphabet
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,23 +3,30 @@
 #include <stdio.h>
 
 /**
- * main - Print Alphabet
+ * main - Print comb
  * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int a;
+	int b;
 
-	for (a = 48; a <= 57; a++)
+	for (a = 48; a < 57; a++)
 	{
-		putchar(a);
-		if (a != 57)
+		for (b = 48; b <= 57; b++)
 		{
-			putchar(',');
-			putchar(' ');
+			if (a != b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != 56 || b != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
-	return (0);
 }
