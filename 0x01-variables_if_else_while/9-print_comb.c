b8125ab60ac11bd39872b714a583a7c781159fce
pyfn <pyfn94@gmail.com>
2023-07-14 13:01:42 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,13 @@
  */
 int main(void)
 {
-	char a;
+	int a;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 48; a <= 57; a++)
 	{
 		putchar(a);
-		if (a != '9')
-			putchar(',');
+		if (a != 57)
+			putchar(','' ');
 	}
 	putchar('\n');
 	return (0);
