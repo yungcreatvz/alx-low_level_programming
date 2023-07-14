cca32d56cc5fe257fb45516a7124105071cf3c00
pyfn <pyfn94@gmail.com>
2023-07-14 11:41:40 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	l = n % 10
+	l = n % 10;
 	/* your code goes there */
 	if (n > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
