085c5a1e98d51b3f41cb312a25e09e8160077c52
pyfn <pyfn94@gmail.com>
2023-07-14 12:08:34 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print Alphabet
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	for (a = 'A'; a <= 'Z'; a++)
+                putchar(a);
+	putchar('\n');
+	return (0);
+}
