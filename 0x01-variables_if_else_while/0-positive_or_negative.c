601e7c14c250700b14fbf7b695c8c018a689946a
pyfn <pyfn94@gmail.com>
2023-07-14 11:15:50 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-		printf("is positive");
+		printf("%d is positive", n);
 	else if (n == 0)
-		printf("is zero");
+		printf("%d is zero", n);
 	else
-		printf("is negative");
+		printf("%d is negative", n);
 	return (0);
 }
