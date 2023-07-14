b956ddaaff06d3e3a78f5da80c9fe7c7f0daf1d1
pyfn <pyfn94@gmail.com>
2023-07-14 11:54:32 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print Alphabet
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		puts ("%c", a);
+	return (0);
+}
