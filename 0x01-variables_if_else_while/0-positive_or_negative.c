48f2d9967658d7862431dbc5f0674aa44f618ad3
pyfn <pyfn94@gmail.com>
2023-07-14 11:13:53 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Check and return if a number is
+ * positive - negative - is zero
+ * betty style doc for function main goes there
+ */
 int main(void)
 {
 	int n;
