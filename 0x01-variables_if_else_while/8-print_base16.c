46b60a1aa57ecfc5edd34eca2b7187274136613a
pyfn <pyfn94@gmail.com>
2023-07-14 12:51:22 -0700

    8-print-base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print Alphabet
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	for (a = 'a'; a >= 'f'; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
