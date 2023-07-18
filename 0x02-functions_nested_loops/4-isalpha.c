0337c779cecc3a92f0b2faadccee062248e81401
pyfn <pyfn94@gmail.com>
2023-07-18 10:22:37 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,5 +9,5 @@
  */
 int _isalpha(int c)
 {
-	return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9');
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'));
 }
