c724ce5b8bb4a1db6a68daa17b7f1c8a6af23652
pyfn <pyfn94@gmail.com>
2023-07-18 07:10:47 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print Put char
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
