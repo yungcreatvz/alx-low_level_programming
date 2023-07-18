811e4e4e39f537fc638a85c28f7e8f40467f7378
pyfn <pyfn94@gmail.com>
2023-07-18 14:41:48 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,13 @@
  */
 int print_last_digit(int n)
 {
-        int a;
+	int a;
 
-        if (n < 0)
-                n= -n;
-        a = n % 10;
+	if (n < 0)
+		n = -n;
+	a = n % 10;
 	if (a < 0)
-                a = -a
-
-        _putchar(a + '0');
-        return (a);
+		a = -a;
+	_putchar(a + '0');
+	return (a);
 }
