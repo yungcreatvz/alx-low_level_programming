99a311d55704cf607d8df8d2b338a043d1dac518
pyfn <pyfn94@gmail.com>
2023-07-18 16:22:37 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,8 +17,11 @@ void times_table(void)
 		{
 			r = n * m;
 			if (r < 10)
+			{
+				_putchar(' ');
 				_putchar(r + '0');
-			else
+			}
+				else
 			{
 				_putchar((r / 10) + '0');
 				_putchar((r % 10) + '0');
