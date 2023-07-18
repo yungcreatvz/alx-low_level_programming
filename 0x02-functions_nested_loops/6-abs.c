fb9ffdc4d858e08af927d07fa48a12e2b0e069c6
pyfn <pyfn94@gmail.com>
2023-07-18 11:39:34 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs  - return absolute integer
+ * @a: character input
+ * followed by a new line
+ * Return: absolute int
+ */
+int _abs(int a)
+{
+	return (a * (-1));
+}
