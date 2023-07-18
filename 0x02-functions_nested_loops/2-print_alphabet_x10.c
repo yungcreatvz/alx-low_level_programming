f83dfa9c1adb7ed9b80254aa311032ebb0f4af12
pyfn <pyfn94@gmail.com>
2023-07-18 09:23:14 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Print Print alphabet function
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char a, i;
+	for (i = 0; i < 10; i++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+			_putchar(a);
+		_putchar('\n');
+	}
+}
