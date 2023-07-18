0a4a876a61c47c87a3f313297fb235b880e352c9
pyfn <pyfn94@gmail.com>
2023-07-18 10:39:29 -0700

    5.Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * sign  - Print check lowercase
+ * @n: character input
+ * followed by a new line
+ * Return: 0 ( is zero) 1 (is positive) -1 (is negative
+ */
+int print_sign(int n);
+{
+	if(n>0)
+		return (1)
+	else if(n == 0)
+		return (0)
+	else 
+		return (-1)
+}
