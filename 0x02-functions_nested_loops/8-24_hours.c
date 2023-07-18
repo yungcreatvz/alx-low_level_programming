590496edb76083615bb54b0d3ab2c4989c15a66d
pyfn <pyfn94@gmail.com>
2023-07-18 15:35:58 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,24 +9,18 @@
  */
 void jack_bauer(void)
 {
-	char  h, h2, m, m2;
+	int h, m;
 
-	for (h = '0'; h <= '2'; h++)
+	for (h = 0; h < 24; h++)
 	{
-		for (h2 = '0'; h2 <= '3'; h2++)
+		for (m = 0; m < 59; m++)
 		{
-			for (m = '0'; m <= '5'; m++)
-			{
-				for (m2 = '0'; m2 <= '9'; m2++)
-				{
-					_putchar(h);
-					_putchar(h2);
-					_putchar(':');
-					_putchar(m);
-					_putchar(m2);
-					_putchar('\n');
-				}
-			}
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0'));
+			_putchar((m % 10) + '0'));
+			_putchar('\n');
 		}
 	}
 }
