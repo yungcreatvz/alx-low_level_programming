cae34a4d7fe55e9430787762a2ab38e57bef3fdb
pyfn <pyfn94@gmail.com>
2023-07-18 09:32:59 -0700

    4-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - Print check lower char function
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	return(c >= 'a' && c <= 'z';)
+}
