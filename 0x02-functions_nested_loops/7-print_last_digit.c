953b44f87c4a46bf1ccab286f3b4a2e14e205616
pyfn <pyfn94@gmail.com>
2023-07-18 12:01:38 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit  - print last number of input
+ * @n: value  input
+ * followed by a new line
+ * Return: int last digit of a var n
+ */
+int print_last_digit(int n)
+{
+		return (n % 10);
+}
