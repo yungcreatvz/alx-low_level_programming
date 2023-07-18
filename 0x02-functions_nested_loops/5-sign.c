188f2cd7e681bfb4ea06c7e02c9496b2347fae25
pyfn <pyfn94@gmail.com>
2023-07-18 10:42:21 -0700

    5.Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,10 +9,10 @@
  */
 int print_sign(int n);
 {
-	if(n>0)
-		return (1)
-	else if(n == 0)
-		return (0)
-	else 
-		return (-1)
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
 }
