a0e4f062ff72abbd2d58576af6cf93974f874aec
pyfn <pyfn94@gmail.com>
2023-07-18 18:02:09 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table  - print n times table
+ * starting from 0
+ * @n: end table
+ * followed by a new line
+ * Return: table per line
+ */
+void print_times_table(int n);
+{
+	int j, m, r;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (m = 0; m <= n; m++)
+		{
+			r = n * m;
+			if (r < 10)
+			{
+				if (m != 0)
+					_putchar(' ');
+				_putchar(r + '0');
+			}
+				else
+			{
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+			}
+			if (m != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
