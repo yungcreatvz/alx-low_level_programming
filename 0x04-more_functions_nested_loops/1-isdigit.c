d9daaac40d46142905058559b4c36b34c07e8663
pyfn <pyfn94@gmail.com>
2023-07-20 06:12:28 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - Print check digits
+ * @c: character input
+ * followed by a new line
+ * Return: 0 (Success) 1 (error)
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
