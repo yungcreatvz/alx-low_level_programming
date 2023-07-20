dd62be6dafb72be06c0f7188947e5668276ad4ca
pyfn <pyfn94@gmail.com>
2023-07-20 06:15:23 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _mul - Print check digits
+ * @a: number 1 input
+ * @b: number 2 input
+ * followed by a new line
+ * Return: result
+ */
+int mul(int a, int b);
+{
+	return (a * b);
+}
