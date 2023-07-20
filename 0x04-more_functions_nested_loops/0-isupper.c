f17f8e5cba051abc4d8689499b71079d63235b3c
pyfn <pyfn94@gmail.com>
2023-07-20 06:06:31 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - Print check uppercase
+ * @c: character input
+ * followed by a new line
+ * Return: 0 (Success) 1 (error)
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
