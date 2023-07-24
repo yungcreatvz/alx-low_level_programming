f590a0aafb34ee88ad3a2bbab69cd3ec10609ed2
pyfn <pyfn94@gmail.com>
2023-07-24 12:48:40 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * swap_int - swap value of two given integer
+ * @a: first received value
+ * @b: second received value
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b, *b = *a;
+}
