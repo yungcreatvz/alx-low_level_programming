08f1ff4e00f9e74b99fd2f370e464e1dd17c6b1f
pyfn <pyfn94@gmail.com>
2023-07-24 13:50:18 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - return lengh of a string
+ * @s: first received value
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	while(*s[i] != '\0')
+		i++;
+	return (i);
+}
