73d10282cf3873d158dd631f63e527558f908870
pyfn <pyfn94@gmail.com>
2023-07-24 16:45:14 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - print a string in reverse
+ * @s: first received value
+ * followed by a new line
+ */
+void rev_string(char *s)
+{
+	int  i = 0, j;
+
+	while (s[i] != '\0')
+		i++;
+
+	char a[i];
+
+	for (j = 0; j < i; j++)
+	{
+		a[j] = s[i - 1];
+		i--;
+	}
+	_putchar('\n');
+}
