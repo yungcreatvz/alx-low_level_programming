8cf76e52a48c43a1f72791ea47b03703856e85ad
pyfn <pyfn94@gmail.com>
2023-07-24 16:20:22 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,11 @@
  */
 void print_rev(char *s)
 {
-	int  i = 0;
+	int  i = 0, j;
 
 	while (s[i] != '\0')
-	{
 		i++;
-	}
-	while (i > 0)
-	{
-	       _putchar(s[i - 1]);
-	       i--;
-	}
+	for (j = i; i > 0; i--)
+		_putchar(s[i - 1]);
 	_putchar('\n');
 }
