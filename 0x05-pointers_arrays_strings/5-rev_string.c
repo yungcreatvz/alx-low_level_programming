b88274ad39722418000225e7f3f0cb37532a30e0
pyfn <pyfn94@gmail.com>
2023-07-24 17:22:49 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,18 +8,16 @@
 void rev_string(char *s)
 {
 	int  i = 0, j;
-	char a[500];
+	char a;
 
 	while (s[i] != '\0')
 		i++;
-	for (j = 0; j <= i; j++)
+
+	for (j = 0; j < i / 2; j++)
 	{
-		if (j == i)
-			a[j] = '\0';
-		else
-			a[j] += s[i - 1];
-		i--;
+		a = s[j];
+		a[j] = s[i - 1];
+		s[i - 1] = a
 	}
-	*s = *a;
 	_putchar('\n');
 }
