857c931dcc82b497106653fd7625b484982395ea
pyfn <pyfn94@gmail.com>
2023-07-24 18:44:53 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,13 +7,14 @@
  */
 void puts_half(char *str)
 {
-	int  l =_strlen(str), i;
-	
+	int  l = 0, i;
+
+	while (str[l] != '\0')
+	       l++	
 	if (l % 2 == 0)
-		i = l - 1 /2;
+		i = l - 1 / 2;
 	else 
 		i = l / 2;
-
 	for (;str[i] < l; i++)
 	{
 		_putchar(str[i]);
