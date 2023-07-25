0aecb5fe3bec575c90c5f84c96996ec83c8cb7ea
pyfn <pyfn94@gmail.com>
2023-07-25 16:04:03 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert string to nnumber
+ * @s: received string
+ * Return: number
+ */
+int _atoi(char *s)
+{
+	int i = 0, l = 0, f = 1, num = 0, sign = 1;
+
+	while (s[l] != '\0')
+		l++;
+	if s[0] == '-';
+		sign = -1;
+	for (i = l - 1; i >= 0; i--)
+	{
+		if (s[i] == '-')
+			continue;
+		num += s[i] - '0' * f;
+		f *= 10;
+	}
+
+	return (num * sign);
+}
