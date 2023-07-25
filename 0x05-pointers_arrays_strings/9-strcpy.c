159e9d3b7409bb5ff97b57cc3b18f1e28f99f235
pyfn <pyfn94@gmail.com>
2023-07-24 19:41:17 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copy string pointed to by src
+ * @dest: paste destinaation
+ * @src: string to copy
+  */
+char *_strcpy(char *dest, char *src)
+{
+	dest = *src;
+}
