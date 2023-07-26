ef767757094a03670017a4f0970a60fa1eeee6ec
pyfn <pyfn94@gmail.com>
2023-07-26 11:14:23 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -24,10 +24,7 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[j] = src[i];
 		j++;
 	}
-	while (i < n)
-	{
-		dest[i] = '\n';
-		i++;
-	}
+	if (i < n)
+		dest[i] = '\0';
 	return (dest);
 }
