6ae52fa17a32891031c624fe79e3f79b1471367d
pyfn <pyfn94@gmail.com>
2023-07-27 16:18:42 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,12 +9,14 @@
 char *leet(char *s)
 {
 	int i, j, k;
-	char l[] = "oOlLeEaAtT", char e[] = "0011334477";
+	char l[] = "oOlLeEaAtT";
+	char e[] = "0011334477";
 
 	i = 0;
 	while (s[i] != '\0')
 	{
-		j = 0, k = 0;
+		j = 0;
+		k = 0;
 		while (l[j] != '\0')
 		{
 			if (s[i] == l[j])
