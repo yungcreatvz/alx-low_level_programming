76f2c0a27db6c173e4612f201ee1fa71aa458d06
pyfn <pyfn94@gmail.com>
2023-07-27 16:35:29 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,10 +12,10 @@ char *rot13(char *s)
 
 	while (s[i] != '\0')
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') 
+		while ((s[i] >= 'a' && s[i] <= 'z')
 		|| (s[i] >= 'A' && s[i] <= 'Z'))
 		{
-			if ((s[i] >= 'a' && s[i] <= 'm') 
+			if ((s[i] >= 'a' && s[i] <= 'm')
 			|| (s[i] >= 'A' && s[i] <= 'M'))
 				s[i] += 13;
 			else
