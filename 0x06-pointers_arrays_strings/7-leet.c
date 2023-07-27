9aebf006697d53877f98b3017e78b916864d545a
pyfn <pyfn94@gmail.com>
2023-07-27 16:16:16 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet -  encodes into 1337.
+ * @s: string
+ * Return: string.
+ */
+char *leet(char *s)
+{
+	int i, j, k;
+	char l[] = "oOlLeEaAtT", char e[] = "0011334477";
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0, k = 0;
+		while (l[j] != '\0')
+		{
+			if (s[i] == l[j])
+			{
+				k = j;
+				s[i] = e[k];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
