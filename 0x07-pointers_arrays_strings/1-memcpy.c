6cb847c777d333522e5aa1119c994cb917ddba8f
yungcreatvz <pyfn94@gmail.com>
2023-07-31 10:12:41 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _memset - copies memory are
+ * _memcpy - copies memory are
  * @dest: detination of src input
  * @src: source to insert in dest
  * @n: range for input
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	for ( i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
