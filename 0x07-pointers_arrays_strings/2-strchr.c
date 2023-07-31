cf78be998d7a6cdd0bb6f1524f5a51a6c15743b4
yungcreatvz <pyfn94@gmail.com>
2023-07-31 10:37:41 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,11 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
-		if (s[i] == c)
-			return (s[i]);
-		i++;
+		if (*s == c)
+			return (s);
+		*(s + 1);
 	}
 	return (NULL);
 }
