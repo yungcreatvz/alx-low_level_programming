0fcc0ebbbb0681e0ab5a10b2a8219c9ee7fd426a
yungcreatvz <pyfn94@gmail.com>
2023-08-02 14:51:23 -0700

    0x08. C - Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print 
+ * Return: next string or 1 at end of strings
+ */
+void _puts_recursion(char *s)
+{
+	if (s != '\0')
+		return (1);
+	puts(s);
+	puts('\n');
+	return ( _puts_recursion(s + 1);
+}
