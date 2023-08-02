59bf385ecc795b760e96b2f56aa99c392c074f19
yungcreatvz <pyfn94@gmail.com>
2023-08-02 14:57:44 -0700

    0x08. C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,9 +7,9 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (s != '\0')
 		return (1);
-	puts(*s);
+	puts(s);
 	puts('\n');
-	return (_puts_recursion(*(s + 1));
+	return (_puts_recursion(s + 1);
 }
