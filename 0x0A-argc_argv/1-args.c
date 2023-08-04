7180d2ee98283b497144bbf7c7c0a797f0bc400c
yungcreatvz <pyfn94@gmail.com>
2023-08-04 12:00:55 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of parameters
+ * @argv: array of given parameters
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
