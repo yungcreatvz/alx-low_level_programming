fb7043d4d03fe19384583f5525f6fcf7f60ff141
yungcreatvz <pyfn94@gmail.com>
2023-08-04 17:05:39 -0700

    bash library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * _puts - print a new string to stdout
- * @str: first received value
- * followed by a new line
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
  */
 void _puts(char *str)
 {
-	int  i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
-		i++;
 	}
 	_putchar('\n');
 }
