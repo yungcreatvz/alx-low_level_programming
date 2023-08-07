1f89cb23adde301bc97da1d7f45aaead86ba5aa2
yungcreatvz <pyfn94@gmail.com>
2023-08-07 09:04:12 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,20 +2,21 @@
 #include <stdlib.h>
 
 /**
- * create_array -  creates an array of chars, and initializes it with a specific char
+ * create_array - creates an array of chars
+ * and initializes it with a specific char
  * @size: lenght off the array
  * @c: charter to initiallizze
- * Return: Null if size is 0 or table of array 
+ * Return: Null if size is 0 or table of array
  */
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
 
 	arr = malloc(size * sizeof(char));
-	if (size == 0)
+	if (size == 0 || arr == NULL)
 		return (NULL);
 	arr[0] = c;
+	arr[size - 1] = '\0';
 	return (arr);
-	
 }
 
