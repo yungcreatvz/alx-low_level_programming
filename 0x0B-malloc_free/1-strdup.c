2d85cfb6f25eb7af5b43e74dd04e7f5861b74741
yungcreatvz <pyfn94@gmail.com>
2023-08-07 09:49:23 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space
+ * @str: charter to initiallizze
+ * Return: Null if fail and pointer if success
+ */
+char *_strdup(char *str)
+{
+	int i = 0, j;
+	char *a;
+
+	while (str[i] != '\0')
+		i++;
+	a = malloc(i * sizeof(char));
+	if (i == 0 || a == NULL)
+		return (NULL);
+	for (j = 0; j < i; j++)
+		a[j] = str[j};
+
+	return (arr);
+}
+
