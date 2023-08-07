64f2618b886f6b66a91847667522183f00e26088
yungcreatvz <pyfn94@gmail.com>
2023-08-07 11:04:35 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: Null if fail or pointer if success
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i = 0, j = 0, len, k = 0, l = 0;
+	char *a;
+
+	if (s1 == NULL)
+		s1 == "";
+	if (s2 == NULL)
+		s2 == "";
+	while (s1[i] != '\0')
+		i++;
+	while (s1[j] != '\0')
+		j++;
+	len = i + j;
+	a = malloc(((len + 1) * sizeof(char));
+	for (k = 0; k <= i; k++)
+		a[k] = s1[k];
+	for (l = 0; k < len; l++)
+	{
+		a[k} = s2[l];
+		k++;
+	}
+
+	return (a);
+}
+
