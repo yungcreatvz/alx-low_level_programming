564bea10fcf9cdccd001b9784504a25d4f7aca1b
yungcreatvz <pyfn94@gmail.com>
2023-08-10 08:06:35 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,7 +9,7 @@
  * @n: lenght of string input from s2
  * Return: a pointer to the allocated memory or NULL
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0, j = 0, k = 0, l = 0, limit;
 	char *a;
