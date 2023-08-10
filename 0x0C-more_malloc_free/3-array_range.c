46dea28935a799977b6aa4dbd49412f1a09c8ba8
yungcreatvz <pyfn94@gmail.com>
2023-08-10 10:03:28 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,7 +12,7 @@ int *array_range(int min, int max)
 	unsigned int i;
 	int *a;
 
-	if (min > max || a == NULL)
+	if (min > max)
 		return (NULL);
 	a = malloc((max - min + 1) * sizeof(int));
 	if (a == NULL)
