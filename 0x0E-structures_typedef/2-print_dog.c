aaba5039836c886441cd79c016ffffc84e20cf2b
yungcreatvz <pyfn94@gmail.com>
2023-08-14 08:23:40 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog -  print a struct dog
+ * @d: the strut to fill
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+                d->owner = "(nil)";
+	printf("Name: %s \n", d->name);
+	printf("Age: %d \n", d->age);
+	printf("owner: %s \n", d->owner);
+	
+}
