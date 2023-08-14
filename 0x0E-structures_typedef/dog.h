fa47ad49db69a3ec773940851e2863ff6f56d89d
yungcreatvz <pyfn94@gmail.com>
2023-08-14 07:31:50 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,10 +15,4 @@ struct dog
 	char *owner;
 };
 
-/* init of functions prototypes */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
 #endif
