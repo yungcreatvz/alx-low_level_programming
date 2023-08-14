f07f2341294f7554b067e3ea968dd60fbfa8df0f
yungcreatvz <pyfn94@gmail.com>
2023-08-14 07:09:14 -0700

    dog.h

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+include "main.h"
+
+/**
+ * init_dog -  initialize a variable of type struct dog
+ * @d: the strut to fill
+ * @name: input for namee case
+ * @age: input for age
+ * @owner: input gott owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+}
