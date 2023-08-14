7609c2c866a52cb354f5b130cce56f52ba6c9d4b
yungcreatvz <pyfn94@gmail.com>
2023-08-14 11:35:48 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free dog
+ * @d: the struct to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+		free(d);
+	}
+}
