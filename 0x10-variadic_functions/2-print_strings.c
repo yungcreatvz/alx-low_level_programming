08e711f1155f320dd475d5505a495b715ff5c60b
yungcreatvz <pyfn94@gmail.com>
2023-08-17 11:27:37 -0700

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings, followed by a new line.
+ * @n: number of given input
+ * @separator: separator between input
+ */
+void print_strings(const char *separator, const unsigned int n, ...);
+{
+	va_list sl;
+	char *str;
+	unsigned int i;
+
+	va_start(sl, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(list, char *);
+		if (!str)
+			str = "(nil)";
+		if (!separator)
+			printf("%s", str);
+		else if (separator && i == 0)
+			printf("%s", str);
+		else
+			printf("%s%s", separator, str);
+	}
+	printf("\n");
+	va_end(sl);
+}
