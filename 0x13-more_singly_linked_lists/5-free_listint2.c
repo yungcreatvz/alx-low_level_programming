5eebceac557fa1b757641822da9dab6646c4c3d2
pyfn <pyfn94@gmail.com>
2023-08-28 19:09:46 +0000

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to the first element of the list
+ * Return: an empty node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+	head = NULL;
+}
