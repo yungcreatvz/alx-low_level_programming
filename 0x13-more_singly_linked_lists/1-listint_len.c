76c4aa6a6c91693387279f4813e92d6454a22832
pyfn <pyfn94@gmail.com>
2023-08-28 17:32:44 +0000

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * listint_len -  returns the number of elements in a linked 
+ * listint_len -  returns the number of elements in a linked
  * @h: pointer to the list
- * Return: size of the list
+ * Return: number of node
  */
-size_t listint_len(const listint_t *h);
+size_t listint_len(const listint_t *h)
 {
 	size_t size = 0;
 
