0f55c4c2a71edeff40a2b3ce4c386b4935755359
pyfn <pyfn94@gmail.com>
2023-08-28 18:21:32 +0000

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,7 +8,7 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *temp, current;
+	listint_t *temp, *current;
 
 	temp = malloc(sizeof(listint_t));
 	if (!temp)
@@ -20,7 +20,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 	{
 		current = *head;
-		while (current)
+		while (current->next)
 			current = current->next;
 		current->next = temp;
 	}
