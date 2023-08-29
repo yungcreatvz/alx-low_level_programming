80aedee02f968eb57ca924fa525f41fd5e9dba1b
pyfn <pyfn94@gmail.com>
2023-08-29 17:37:09 +0000

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -13,22 +13,27 @@ size_t free_listint_safe(listint_t **h)
 
 	if (!h || !*h)
 		return (0);
+
 	while (*h)
 	{
 		diff = *h - (*h)->next;
 		if (diff > 0)
 		{
 			temp = (*h)->next;
+			free(*h);
 			*h = temp;
 			len++;
 		}
 		else
 		{
+			free(*h);
 			*h = NULL;
 			len++;
 			break;
 		}
 	}
+
 	*h = NULL;
+
 	return (len);
 }
