4a1bbd0dbcb1a7b16acdff4e2f86951f87fb37a8
pyfn <pyfn94@gmail.com>
2023-08-29 16:13:02 +0000

    9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - Insert an element at idx positiion.
+ * @head: First node address.
+ * @idx: Position to place element.
+ * @n: element to insert.
+ * Return: the new node.
+ **/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *temp;
+	unsigned int i = 0;
+
+	if (*head == NULL && idx != 0)
+		return (NULL);
+	if (idx != 0)
+	{
+	temp = *head;
+	for (; i < idx - 1 && temp != NULL; i++)
+		temp = temp->next;
+	if (temp == NULL)
+		return (NULL);
+	}
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
+}
