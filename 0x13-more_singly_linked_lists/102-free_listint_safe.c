6be7e8707f9cebf2dcb6dd734c9ecba9a8170d24
pyfn <pyfn94@gmail.com>
2023-08-29 17:31:47 +0000

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frees a list
+ * @h: first node of the  thelist
+ * Return: number of elements of freed list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
+
+	if (!h || !*h)
+		return (0);
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
+	*h = NULL;
+	return (len);
+}
