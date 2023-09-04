706fbdc8cd858469db51dd1c2d7dc8552706d9ea
yungcreatvz <pyfn94@gmail.com>
2023-09-04 16:22:19 -0700

    1-create_file.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,7 @@
 /**
  * read_textfile - Reads a text file and prints it to POSIX stdout.
  * @filename: A pointer to the name of the file.
- * @letters: The number of letters 
+ * @letters: The number of letters
  * Return: If the function fails or filename is NULL - 0.
  *         O/w - the actual number of bytes the function can read and print.
  */
